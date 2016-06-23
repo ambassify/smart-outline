2bec8371286b9d46029b678361684bc18929ca67
Wim <wim@sitebase.be>
2016-06-23 13:23:20 +0200

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# smart-outline
-Disabling focus outline without breaking accessibility
+# Smart Outline
+Disabling focus outline without breaking accessibility.
+This library will only disable the focus outline when the user is using his mouse
+to navigate. Once a users starts using his keyboard the outline is activated again
+resulting in the best of both worlds, visually and accessibility wise.
+
+## Usage
+This library supports both **AMD** and **CommonJS** module loading and it falls back
+it creates a global `smartOutline` object on window.
+
+### RequireJS
+>var smartOutline = require('smartOutline');
+
+### ES6
+>import smartOutline from 'smartOutline';
+
+## Run demo page
+Execute following command:
+>npm run serve
+
+and navigate to `http://127.0.0.1:7000`.
+
+# Unit Tests
+>npm test
+
+### Initialize
+smartOutline.init();
+
+### Initialize with custom configuration
+>smartOutline.init({
+>    domId: 'my-custom-id'
+>});
+
+## Options
+
+| Option   |      Type      |  Description |
+|----------|:-------------:|------:|
+| domId | `string` | ID that is assigned to the style element that gets injected in the DOM |
+| hideFocusCSS | `string` | Overwrite the default CSS to hide the focus outline when a user is using the mouse |
+
+## Methods
+
+| Method   |      Return      |  Description |
+|----------|:-------------:|------:|
+| .init() | `HTMLElement` | Check is smart outline is active |
+| .isKeyboardUser() | `boolean` | Return `true` if the current user is a keyboard user |
+| .isEnabled() | `boolean` | Check is smart outline is active |
+| .destroy() | `null` | Disable smart outline by remove injected style element and all event listeners |
+
+## Browser Support
+* Chrome
+* Firefox
+* Safari
+* Opera
+* IE 9+
+* Edge
---
+++ main.js
@@ -0,0 +1,123 @@
+'use strict';
+
+/**
+ * function that will add a style block that will hide the focus
+ * outlines while a user is using the mouse to navigate the application,
+ * once we detect that a user is using keyboard navigation (tab) we remove
+ * this style block so that the real focus outlines will be visible again.
+ */
+
+(function (root, factory) {
+    if(typeof define === 'function' && define.amd) { // eslint-disable-line
+        define(factory); // eslint-disable-line
+    } else if(typeof module === 'object' && module.exports) {
+        module.exports = factory();
+    } else {
+        root.smartOutline = factory();
+    }
+}(this, function() {
+
+    var defaultDomId = 'bbbx-a11y-fix';
+    var defaultHideFocusCSS = '*:focus {outline:0 !important;}::-moz-focus-inner{border:0;}';
+    var options = null;
+
+    var KEYCODE_TAB = 9;
+    var KEYCODE_SPACE = 39;
+    var KEYCODE_LEFT = 37;
+    var KEYCODE_UP = 38;
+    var KEYCODE_RIGHT = 39;
+    var KEYCODE_DOWN = 40;
+
+    function ensureInitCalled() {
+        if(options === null)
+            throw new Error('Smart Outline is not initialized yet. Make sure to call `.init()` first.')
+    }
+
+    var smartOutline = {
+        getStyleEl: function() {
+            var opts = options || {};
+            return document.getElementById(opts.domId || defaultDomId);
+        },
+        setCSS: function(css) {
+            this.getStyleEl().innerHTML = css;
+        },
+        _clickListener: function(evt) {
+            smartOutline.setCSS(options.hideFocusCSS);
+            window.removeEventListener('click', smartOutline._clickListener, false);
+            window.addEventListener('keydown', smartOutline._keyDownListener); // eslint-disable-line
+        },
+
+        _keyDownListener: function(evt) {
+
+            // only remove the outline if the user is using one of the keyboard
+            // navigation keys
+            if(
+                evt.keyCode !== KEYCODE_TAB &&
+                evt.keyCode !== KEYCODE_SPACE &&
+                evt.keyCode !== KEYCODE_LEFT &&
+                evt.keyCode !== KEYCODE_RIGHT &&
+                evt.keyCode !== KEYCODE_UP &&
+                evt.keyCode !== KEYCODE_DOWN
+            )
+                return;
+
+            smartOutline.setCSS('');
+            window.removeEventListener('keydown', smartOutline._keyDownListener, false);
+            window.addEventListener('click', smartOutline._clickListener);
+        },
+
+        getOptions: function() {
+            ensureInitCalled();
+            return options;
+        },
+
+        isKeyboardUser: function() {
+            ensureInitCalled();
+            var el = smartOutline.getStyleEl();
+            return this.getStyleEl().innerHTML === '';
+        },
+
+        isEnabled: function() {
+            var el = smartOutline.getStyleEl();
+            return el ? true : false;
+        },
+
+        init: function(userOptions) {
+
+            // create options were user options overwrites default options
+            userOptions = userOptions || {};
+            options = {
+                domId: userOptions.domId || defaultDomId,
+                hideFocusCSS: userOptions.hideFocusCSS || defaultHideFocusCSS
+            };
+
+            // only add style element if it doesn't exist yet
+            if(this.getStyleEl())
+                return this.getStyleEl();
+
+            // only bind the click handler if there is no a11y style element yet
+            window.addEventListener('click', smartOutline._clickListener);
+
+            var head = document.head || document.getElementsByTagName('head')[0];
+            var style = document.createElement('style');
+            style.id = options.domId;
+            style.type = 'text/css';
+
+            if(!head)
+                return false;
+
+            return head.appendChild(style);
+        },
+
+        destroy: function() {
+            ensureInitCalled();
+            var head = document.head || document.getElementsByTagName('head')[0];
+            head.removeChild(smartOutline.getStyleEl());
+            window.removeEventListener('keydown', smartOutline._keyDownListener, false);
+            window.removeEventListener('click', smartOutline._clickListener, false);
+        }
+    };
+
+    return smartOutline;
+}));
+
